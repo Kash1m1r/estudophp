473f6dd7b9fe759ca6f9c6894ffd8ae6f56e0d33
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-03-08 14:54:15 -0300

    poo

---
+++ projeto/src/conta.php
@@ -0,0 +1,10 @@
+<?php
+
+function criarConta(string $cpf, string $nome, float $saldo):array{
+    return [
+        $cpf => [
+            'titular' => $nome,
+            'saldo' => $saldo
+        ]
+    ];
+}
