9b341755826f4f0dfc4c0a2eaf9095fdf0a8988c
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-03-08 11:33:45 -0300

    strings

--- string/strings2.php
+++ string/strings2.php
@@ -1,7 +1,8 @@
 <?php
 
-$email = 'gabriel@gmail.com';
-$senha = '1112111111';
+$nome = 'joão canaã';
+$email = '      gabriel@gmail.com';
+$senha = 'aéeaãao';
 
 echo substr($email, 0, 8);
 $posicao = strpos($email, 'gmail');
@@ -10,10 +11,21 @@ echo substr($email, 0 , $posicao);
 
 echo strlen($senha). PHP_EOL;
 
-if(strlen($senha) < 8){
+if(mb_strlen($senha) < 8){
     echo "Senha com poucos caracteres!". PHP_EOL;
 }else{
     echo "Senha autorizada!". PHP_EOL;
 }
 
 echo strtoupper(($email)) . PHP_EOL;
+echo strtolower($nome. PHP_EOL);
+
+list($nome, $sobrenome) = explode(' ', $nome);
+
+echo 'Nome:' . $nome .PHP_EOL;
+echo 'Sobrenome:' . $sobrenome .PHP_EOL;
+
+$csv = 'Gabriel Pereira,24,gabrielgomes@ma.def.br';
+var_dump(explode(',',$csv));
+
+echo trim($email);
